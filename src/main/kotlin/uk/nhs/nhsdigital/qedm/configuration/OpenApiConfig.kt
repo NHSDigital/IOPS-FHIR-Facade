1139b2b1472e9df531321990e3178976e1b450fe
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2022-11-18 05:36:59 +0000

    Added CORS support

--- src/main/kotlin/uk/nhs/nhsdigital/qedm/configuration/OpenApiConfig.kt
+++ src/main/kotlin/uk/nhs/nhsdigital/qedm/configuration/OpenApiConfig.kt
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.models.media.StringSchema
 import io.swagger.v3.oas.models.parameters.Parameter
 import io.swagger.v3.oas.models.responses.ApiResponse
 import io.swagger.v3.oas.models.responses.ApiResponses
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -50,7 +51,9 @@ open class OpenApiConfig {
                     .termsOfService("http://swagger.io/terms/")
                     .license(License().name("Apache 2.0").url("http://springdoc.org"))
             )
-
+        oas.addServersItem(
+            Server().description(fhirServerProperties.server.name).url(fhirServerProperties.server.baseUrl)
+        )
 
         // Tags
 
