567995fbfd9cabc529885275514b9b0fa6cd1ce3
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2023-02-09 17:10:06 +0000

    Binary url fix

--- src/main/kotlin/uk/nhs/england/qedm/provider/DocumentReferenceProvider.kt
+++ src/main/kotlin/uk/nhs/england/qedm/provider/DocumentReferenceProvider.kt
@@ -7,11 +7,14 @@ import ca.uhn.fhir.rest.param.TokenParam
 import ca.uhn.fhir.rest.server.IResourceProvider
 import org.hl7.fhir.r4.model.*
 import org.springframework.stereotype.Component
+import uk.nhs.england.qedm.configuration.FHIRServerProperties
 import uk.nhs.england.qedm.interceptor.CognitoAuthInterceptor
 import javax.servlet.http.HttpServletRequest
 
 @Component
-class DocumentReferenceProvider(var cognitoAuthInterceptor: CognitoAuthInterceptor) : IResourceProvider {
+class DocumentReferenceProvider(var cognitoAuthInterceptor: CognitoAuthInterceptor,
+                                val  fhirServerProperties: FHIRServerProperties
+) : IResourceProvider {
     override fun getResourceType(): Class<DocumentReference> {
         return DocumentReference::class.java
     }
@@ -20,9 +23,22 @@ class DocumentReferenceProvider(var cognitoAuthInterceptor: CognitoAuthIntercept
     @Read
     fun read(httpRequest : HttpServletRequest, @IdParam internalId: IdType): DocumentReference? {
         val resource: Resource? = cognitoAuthInterceptor.readFromUrl(httpRequest.pathInfo, null)
-        return if (resource is DocumentReference) resource else null
+        return if (resource is DocumentReference) fixUrl(resource) else null
     }
 
+    fun fixUrl(documentReference: DocumentReference) : DocumentReference {
+        if (documentReference.hasContent() ) {
+            for (content in documentReference.content) {
+                if (content.hasAttachment() && content.attachment.hasUrl()) {
+                    if (content.attachment.url.startsWith("http://localhost:")) {
+                        var urls = content.attachment.url.split("Binary")
+                        if (urls.size>1) content.attachment.url = fhirServerProperties.server.baseUrl + "/FHIR/R4"+ urls[1]
+                    }
+                }
+            }
+        }
+        return documentReference
+    }
     @Search
     fun search(
         httpRequest : HttpServletRequest,
@@ -36,7 +52,7 @@ class DocumentReferenceProvider(var cognitoAuthInterceptor: CognitoAuthIntercept
         val resource: Resource? = cognitoAuthInterceptor.readFromUrl(httpRequest.pathInfo, httpRequest.queryString)
         if (resource != null && resource is Bundle) {
             for (entry in resource.entry) {
-                if (entry.hasResource() && entry.resource is DocumentReference) healthcareServices.add(entry.resource as DocumentReference)
+                if (entry.hasResource() && entry.resource is DocumentReference) healthcareServices.add(fixUrl(entry.resource as DocumentReference))
             }
         }
 
